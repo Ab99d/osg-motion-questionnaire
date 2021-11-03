9bf2171460697f07a0b2f34f9226ad97bfd7af98
AbdulrahmanTaha <aalshami665@gmail.com>
2021-11-03 03:56:00 -0800

    first submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import MotionQuestionnaireEn from './pages/MotionQuestionnaireEn'
+import MotionQuestionnaireAr from './pages/MotionQuestionnaireAr'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={MotionQuestionnaireEn} />
+          <Route path="/ar" component={MotionQuestionnaireAr} />
+        </div>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
